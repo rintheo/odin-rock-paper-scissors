f0eb95fa2cce4cf6a7ada3448da22737554be873
rintheo <107115767+rintheo@users.noreply.github.com>
2023-06-19 19:40:35 +0800

    Complete game script

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
-let playerScore;
+let playerScore = 0;
+let computerScore = 0;
 let playerSelection;
-let computerScore;
 let computerSelection;
 
-function capitalize(str){
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+const screen = document.querySelector('.screen');
+const screenTextBig = document.querySelector('#screenTextBig');
+const screenTextSmall = document.querySelector('#screenTextSmall');
+const buttons = document.querySelectorAll('.button');
+const playerScoreText = document.querySelector('#playerScore');
+const computerScoreText = document.querySelector('#computerScore');
+
+function gameStart(e) {
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
+    if (playerScore < 3 && computerScore < 3) {
+        buttons.forEach(makeButtonActive);
+        screenTextBig.classList.add('hidden');
+        screenTextSmall.textContent = 'Rock, Paper or Scissors!?';
+        screen.removeEventListener(e.type, gameStart); 
+    }
+    else {
+        if (playerScore >= 3) {
+            screenTextBig.textContent = 'Winner!';
+            screenTextSmall.textContent = `You won against CPU. Play again?`;
+        }
+        else if (computerScore >= 3) {
+            screenTextBig.textContent = 'Game Over';
+            screenTextSmall.textContent = `You lost against CPU. Try again?`;
+        }
+        playerScore = 0;
+        computerScore = 0;
+        buttons.forEach(makeButtonInactive);
+    }
 }
 
-function getPlayerChoice(){
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    while (true){
-        let choice = prompt('Rock, Paper, Scissors!?');
+function makeButtonActive(button) {
+    button.classList.remove('selected');
+    button.classList.remove('inactive');
+    button.classList.add('active');
+    button.removeEventListener('click', gameStart)
+    button.addEventListener('click', getPlayerChoice)
+}
 
-        if (choice != null){
-            if (choices.includes(capitalize(choice))){
-                return capitalize(choice);
-            }
-            else {
-                console.log('Invalid choice');
-            }
-        }
-        else {
-            console.log('Canceled');
-        }
+function makeButtonInactive(button) {
+    button.classList.remove('selected');
+    button.classList.remove('active');
+    button.classList.add('inactive');
+    button.removeEventListener('click', gameStart)
+    button.removeEventListener('click', getPlayerChoice)
+}
 
+function highlightButtonSelection(button) {
+    button.classList.remove('active');
+    if (button.getAttribute('data-selection') === playerSelection) {
+        button.classList.add('selected');
     }
+    else {
+        button.classList.add('inactive');
+    }
+    button.removeEventListener('click', getPlayerChoice);
+    button.addEventListener('click', gameStart)
 }
 
 function getComputerChoice(){
@@ -33,9 +68,22 @@ function getComputerChoice(){
     return choices[choice];
 }
 
+function getPlayerChoice(e){
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    playerSelection = this.getAttribute('data-selection');
+    computerSelection = getComputerChoice();
+
+    buttons.forEach(highlightButtonSelection);
+    playRound(playerSelection, computerSelection);
+    
+    screen.addEventListener('click', gameStart);
+}
+
 function playRound(playerSelection, computerSelection){  
+    screenTextBig.classList.remove('hidden');
     if (playerSelection === computerSelection){
-        return `Draw! You both picked ${playerSelection}.`;
+        screenTextBig.textContent = 'Draw!';
+        screenTextSmall.textContent = `You both picked ${playerSelection}.`;
     }
     else if (
         (playerSelection === 'Rock' && computerSelection === 'Paper') || 
@@ -43,7 +91,9 @@ function playRound(playerSelection, computerSelection){
         (playerSelection === 'Scissors' && computerSelection === 'Rock')
         ){
             computerScore += 1;
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+            computerScoreText.classList.add('add-score');
+            screenTextBig.textContent = 'You lose!';
+            screenTextSmall.textContent = `${computerSelection} beats ${playerSelection}.`;
         }
     else if (
         (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
@@ -51,35 +101,19 @@ function playRound(playerSelection, computerSelection){
         (playerSelection === 'Scissors' && computerSelection === 'Paper')
         ){
             playerScore += 1;
-            return `You win! ${playerSelection} beats ${computerSelection}.`
+            playerScoreText.classList.add('add-score');
+            screenTextBig.textContent = 'You win!';
+            screenTextSmall.textContent = `${playerSelection} beats ${computerSelection}.`;
         }
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
 }
 
-function game() {
-    // Log initial scores
-    playerScore = 0;
-    computerScore = 0;    
-    console.log(`Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
-
-    // Best of five games
-    while (playerScore < 3 && computerScore < 3){
-        // Get player choices
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();    
-
-        // Play a round
-        console.clear();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
-
-        if (playerScore >= 3){
-            return 'You win the game!';
-        }
-        else if (computerScore >=3){
-            return 'You lose the game!';
-        }
-    } 
+function removeScoreEffect(e) {
+    this.classList.remove('add-score');
 }
 
-// Play a game of five once
-console.log(game());
+screen.addEventListener('click', gameStart);
+playerScoreText.addEventListener('transitionend', removeScoreEffect);
+computerScoreText.addEventListener('transitionend', removeScoreEffect);
+
